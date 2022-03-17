25cfbbf3fb37481d7b291fceab9313dbeeac3732
Paraparan1 <kanesamoorthyparaparan@gmail.com>
2022-03-17 12:25:22 +0000

    Update main.py
    Added graphs for model accuracy and model loss.
    
    Added confusion matrix for predictions.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
+import matplotlib
 from sklearn.metrics import confusion_matrix
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 from tensorflow.python.keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
+from mlxtend.plotting import plot_confusion_matrix
+from mlxtend.evaluate import confusion_matrix
+
+
 import tensorflow.keras as keras
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -10,7 +15,7 @@ import numpy as np
 import cv2
 
 #Assigning the data directory.
-dataDir = "Combined Dataset"
+dataDir = "Sign Language Dataset 2"
 
 #Assigning each class to the classes list.
 class_names = ["del","spc","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W",
@@ -31,7 +36,6 @@ def preprocessData():
             try:
                 #Converting the size of the image to 60 by 60
                 img_arr2 = cv2.resize(img_arr, dsize=(60,60))
-                print(path,img)
             except Exception as e:
                 #In case of failure of converting a specific image the path is printed of the image with the error.
                 print(path,img)
@@ -68,58 +72,93 @@ X_test = X_test/255.0
 print(X_train[1].shape)
 
 # Setting up a sequential neural network with 7 layers.
-model = tf.keras.Sequential([
-        #A convolutional layer with 16 filters and a kernel with a dimension of 3x3.
-        tf.keras.layers.Conv2D(16, (3,3), activation ='relu', input_shape=X_train[1].shape),
-        #Same layer as first but will learn unique features as the filters is randomised.
-        tf.keras.layers.Conv2D(16, (3,3), activation ='relu'),
-        #Maxpooling is used to reduce spatial dimensions of the output volume.
-        #creates a feature map based of previous map containing important features.
-        tf.keras.layers.MaxPool2D((2,2)),
-        tf.keras.layers.Dense(128, activation="relu"),
-        #flattening the data to a 1 dimensional array for next layer
-        tf.keras.layers.Flatten(),
-        #Dense layer with 50 neurons.
-        tf.keras.layers.Dense(50, activation='relu'),
-        #An output dense layer contain 28 neurons 26 for alphabet and 2 for space and delete gestures.
-        tf.keras.layers.Dense(28, activation='softmax')
-])
+# model = tf.keras.Sequential([
+#         #A convolutional layer with 16 filters and a kernel with a dimension of 3x3.
+#         tf.keras.layers.Conv2D(16, (3,3), activation ='relu', input_shape=X_train[1].shape),
+#         #Same layer as first but will learn unique features as the filters is randomised.
+#         tf.keras.layers.Conv2D(16, (3,3), activation ='relu'),
+#         #Maxpooling is used to reduce spatial dimensions of the output volume.
+#         #creates a feature map based of previous map containing important features.
+#         tf.keras.layers.MaxPool2D((2,2)),
+#         tf.keras.layers.Dense(128, activation="relu"),
+#         #flattening the data to a 1 dimensional array for next layer
+#         tf.keras.layers.Flatten(),
+#         #Dense layer with 50 neurons.
+#         tf.keras.layers.Dense(50, activation='relu'),
+#         #An output dense layer contain 28 neurons 26 for alphabet and 2 for space and delete gestures.
+#         tf.keras.layers.Dense(28, activation='softmax')
+# ])
+#
+# model.compile(optimizer="adam",loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+#
+# #Early stop monitor to prevent over fitting by measuring validation loss once validation loss increases
+# #the neural network will stop training.
+#
+# monitor = EarlyStopping(monitor = "val_loss",min_delta=1e-3,patience=5,verbose=1,mode="auto"
+#                         ,restore_best_weights=True)
+#
+# history = model.fit(X_train,Y_train,validation_data=(X_val,Y_val),callbacks=[monitor],verbose=2,epochs=1000)
 
-model.compile(optimizer="adam",loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+model = keras.models.load_model("ASLModel.h5")
 
-#Early stop monitor to prevent over fitting by measuring validation loss once validation loss increases
-#the neural network will stop training.
-
-
-monitor = EarlyStopping(monitor = "val_loss",min_delta=1e-3,patience=5,verbose=1,mode="auto"
-                        ,restore_best_weights=True)
-
-model.fit(X_train,Y_train,validation_data=(X_val,Y_val),callbacks=[monitor],verbose=2,epochs=1000)
-
-# model = keras.models.load_model("ASLModelCombined5.h5")
-
-model.summary()
 prediction = model.predict(X_val)
 
-font1 = {'family':'serif','color':'blue','size':20}
-font2 = {'family':'serif','color':'black','size':15}
-predTrue = 0
-predFalse= 0
+# model.summary()
+
+##Plotting Model Accuracy over epochs.
+# plt.plot(history.history["accuracy"])
+# plt.plot(history.history["val_accuracy"])
+# plt.title("Model Accuracy")
+# plt.ylabel("Accuracy")
+# plt.xlabel("Epoch")
+# plt.legend(["Train, Val"],loc="upper left")
+#
+# plt.show()
+
+##Ploting Model Loss
+# plt.plot(history.history["loss"])
+# plt.plot(history.history["val_loss"])
+# plt.title("Model Loss")
+# plt.ylabel("Loss")
+# plt.xlabel("Epoch")
+# plt.legend(["Train, Val"],loc="upper left")
+# plt.show()
+
+y_pred = model.predict(X_val)
+
+#Stores all predictions in a list.
+y_predicted = np.argmax(y_pred,axis=-1)
+
+#Uses predictions and true labels to form a confusion matrix.
+cm = confusion_matrix(y_target=Y_val,
+                      y_predicted=y_predicted,
+                      binary=False)
 
 
-for i in range(200):
-    plt.grid(False)
-    plt.title("Prediction Number:"+str(i+1),font1)
-    plt.imshow(X_val[i],cmap="gray")
-    plt.ylabel("Actual: "+class_names[Y_val[i]],font2)
-    plt.xlabel("Prediction: "+ class_names[np.argmax(prediction[i])],font2)
-    if class_names[Y_val[i]] == class_names[np.argmax(prediction[i])]:
-        predTrue+=1
-    else:
-        predFalse+=1
-    plt.show()
+fig, ax = plot_confusion_matrix(conf_mat=cm,figsize=(5,5),class_names=class_names)
+plt.show()
 
-print("Predicted Correct: "+str(predTrue))
-print("Predicted Incorrect: "+str(predFalse))
+#
+#
+# font1 = {'family':'serif','color':'blue','size':20}
+# font2 = {'family':'serif','color':'black','size':15}
+# predTrue = 0
+# predFalse= 0
+#
+#
+# for i in range(200):
+#     plt.grid(False)
+#     plt.title("Prediction Number:"+str(i+1),font1)
+#     plt.imshow(X_val[i],cmap="gray")
+#     plt.ylabel("Actual: "+class_names[Y_val[i]],font2)
+#     plt.xlabel("Prediction: "+ class_names[np.argmax(prediction[i])],font2)
+#     if class_names[Y_val[i]] == class_names[np.argmax(prediction[i])]:
+#         predTrue+=1
+#     else:
+#         predFalse+=1
+#     plt.show()
+#
+# print("Predicted Correct: "+str(predTrue))
+# print("Predicted Incorrect: "+str(predFalse))
 
 # model.save("ASLmodelCombined5.h5")
